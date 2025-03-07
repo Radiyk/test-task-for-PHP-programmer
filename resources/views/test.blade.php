a758c7a0f2723643488c8583a03ac6d91c6cdf69
alexey <kabictan@gmail.com>
2025-03-07 19:02:01 +0300

    new

---
+++ resources/views/test.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>TEST</title>
+
+    </head>
+    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        <h1 class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+              klklklk
+        </h1>
+
+    </body>
+</html>
