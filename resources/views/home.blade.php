83f7718d2057aab9c05c80a25f431549d8da9787
alexey <kabictan@gmail.com>
2025-03-14 19:03:05 +0300

    Приложение добавлены сиды

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,9 @@
     <div class="flex items-center justify-center ">
         <h1 class="mb-1 font-medium">управление товарами и заказами</h1>
     </div>
+    <div class="flex items-center justify-start ">
+        <p class="mb-1 font-medium">Если в таблицах нат данных, возможно необходимо <br>запустить миграции таблиц сиды для их наполнения.</p>
+        <p class="mb-1 font-medium">php artisan migrate</p>
+        <p class="mb-1 font-medium">php artisan db:seed</p>
+    </div>
 @endsection
